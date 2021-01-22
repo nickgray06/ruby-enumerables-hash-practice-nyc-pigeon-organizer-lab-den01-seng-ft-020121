d82e7f17ae934f67f94440c267bc42de6b5f7c8c
Nick Gray <nickgray87@gmail.com>
2021-01-22 23:53:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,12 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key,value), final_data|
-    binding.pry
     value.each do |inner_key, names|
-      names.each do |names|
+      names.each do |name|
+        if !final_data[name]
+          final_data[name] = {}
+        end
+      end
     end
     final_data
   end

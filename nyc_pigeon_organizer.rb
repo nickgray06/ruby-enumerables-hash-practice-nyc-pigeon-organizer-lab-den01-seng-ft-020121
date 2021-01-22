4e7a91836dfa377f16912afd89ee9fd2d65dab00
Nick Gray <nickgray87@gmail.com>
2021-01-22 23:46:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key,value), final_data|
     binding.pry
+    value.each do |inner_key, names|
     final_data
   end
 end

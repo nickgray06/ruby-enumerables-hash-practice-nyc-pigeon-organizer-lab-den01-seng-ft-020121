11906647c9e3364107346683d55dc6025850256e
Nick Gray <nickgray87@gmail.com>
2021-01-23 00:02:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
         if !final_data[name][key]
           !final_data[name][key] = []
         end
-        final_data[name][key].push(inner_key)
+        final_data[name][key].push(inner_key.to_s)
       end
     end
   end

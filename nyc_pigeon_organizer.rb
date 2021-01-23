219a34b325cbb0857f8cc47a54fc8a465b7c25d3
Nick Gray <nickgray87@gmail.com>
2021-01-23 00:02:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key,value), final_data|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_data[name]
+          final_data[name] = {}
+        end
+        if !final_data[name][key]
+          !final_data[name][key] = []
+        end
+        final_data[name][key].push(inner_key.to_s)
+      end
+    end
+  end
 end
